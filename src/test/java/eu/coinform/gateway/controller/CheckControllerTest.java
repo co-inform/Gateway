2bcda7130a51475e2da94edad388275bca986ba5
daffyDukk <daniel.p.forsberg@gmail.com>
2019-09-25 10:30:41 +0200

    first test

--- src/test/java/eu/coinform/gateway/controller/CheckControllerTest.java
+++ src/test/java/eu/coinform/gateway/controller/CheckControllerTest.java
@@ -1,5 +1,6 @@
 package eu.coinform.gateway.controller;
 
+import static org.mockito.ArgumentMatchers.contains;
 import static org.mockito.Mockito.when;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -10,13 +11,18 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.Resource;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.junit.Assert.*;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -30,29 +36,34 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class CheckControllerTest {
 
     ObjectMapper mapper = new ObjectMapper();
-    String jsonTU;// = "{\"user_id\": \"25073877\", \"screen_name\": \"realDonaldTrump\"}";
+    String jsonTU;
     final String userId = "25073877";
     final String screenName = "realDonaldTrump";
 
     @MockBean
     private CheckController checkController;
 
-//    @Autowired
     TwitterUser twitterUser = new TwitterUser();
 
     @Autowired
     private MockMvc mockMvc;
 
+    Resource<Check> rc;
+
     @Before
     public void setupTests(){
-        System.out.println("setupTests: " + twitterUser.toString());
+        log.debug("setupTests: " + twitterUser.toString());
         twitterUser.setScreenName(screenName);
         twitterUser.setTwitterId(userId);
-        System.out.println("setupTests: " + twitterUser.toString());
+        rc = new Resource<>(twitterUser,
+                linkTo(methodOn(CheckController.class).findById(twitterUser.getId())).withSelfRel()
+        );
+        log.debug("RC {}", rc.toString());
+        log.debug("setupTests: " + twitterUser.toString());
         try {
             jsonTU = mapper.writeValueAsString(twitterUser);
-            System.out.println("setupTests: " + twitterUser.toString());
-            System.out.println("setupTests: " + jsonTU);
+            log.debug("setupTests: " + twitterUser.toString());
+            log.debug("setupTests: " + jsonTU);
             log.debug("Json: " + jsonTU);
         } catch (JsonProcessingException e) {
             log.error("Error: " + e.getMessage());
@@ -68,19 +79,18 @@ public class CheckControllerTest {
     @Test
     public void twitterUser() throws Exception{
         log.debug("In twitteruser test");
-//        System.out.println(jsonTU);
-        System.out.println("Twitteruser: " + twitterUser.toString());
-        Resource<Check> rc = checkController.twitterUser(twitterUser); //rc == null?
-        //assertThat(rc).isNotNull(); // är jävligt mykke null...
-        //String s = mapper.writeValueAsString(rc.toString());
-        //System.out.println("RC vaddå?: " + s);
-        when(checkController.twitterUser(twitterUser)).thenReturn(rc);
+
+        log.debug("Twitteruser: " + twitterUser.toString());
+        String id = twitterUser.getId();
+        assertThat(rc).isNotNull(); // är jävligt mykke null...
+
+        when(checkController.twitterUser(Mockito.any(TwitterUser.class))).thenReturn(rc);
         mockMvc.perform(post("/twitter/user")
                         .contentType(MediaType.APPLICATION_JSON_UTF8)
                         .content(jsonTU)
                         .accept(MediaType.APPLICATION_JSON_UTF8))
                 .andDo(print()).andExpect(status().isOk())
-                .andExpect(content().string("")); //WTF??
+                .andExpect(content().json(jsonTU));
     }
 
     @Test
