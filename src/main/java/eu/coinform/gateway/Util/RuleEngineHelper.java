d13cd843efa1e7ecbf4d1a1e035e04a02ee13630
Andreas <andreas.berg@dsv.su.se>
2019-10-10 11:33:28 +0200

    Helper function to map ModuleResponses to Facts for the RuleEngine

---
+++ src/main/java/eu/coinform/gateway/Util/RuleEngineHelper.java
@@ -0,0 +1,73 @@
+package eu.coinform.gateway.Util;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import eu.coinform.gateway.cache.ModuleResponse;
+import eu.coinform.gateway.cache.ModuleTransaction;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class RuleEngineHelper {
+
+    /**
+     * Populates a map with a flattening of the ModuleResponse.
+     *
+     * @param response The ModuleResponse to flatten
+     * @param outputMap The map to populate with the flattened map
+     * @param baseKey The base of the keys to add
+     */
+    public static void flatResponseMap(ModuleResponse response, Map<String, Object> outputMap, String baseKey) {
+        new ResponseParserObject(response.getResponse(), outputMap, baseKey + ".");
+    }
+
+    private static class ResponseParserObject {
+
+        ResponseParserObject(LinkedHashMap<String, Object> jsonObject, Map<String, Object> flatMap, String keyBase) {
+
+            if(jsonObject == null) {
+                return;
+            }
+
+            for (Map.Entry<String, Object> entry : jsonObject.entrySet()) {
+                if (entry.getValue() instanceof LinkedHashMap) {
+                    new ResponseParserObject((LinkedHashMap<String, Object>) entry.getValue(),
+                            flatMap,
+                            String.format("%s%s.", keyBase, entry.getKey().toLowerCase()));
+                } else if (entry.getValue() instanceof ArrayList) {
+                    new ResponseParserArray((ArrayList<Object>) entry.getValue(),
+                            flatMap,
+                            String.format("%s%s", keyBase, entry.getKey().toLowerCase()));
+                } else {
+                    flatMap.put(String.format("%s%s", keyBase, entry.getKey().toLowerCase()), entry.getValue());
+                }
+            }
+        }
+    }
+
+    private static class ResponseParserArray {
+
+        ResponseParserArray(ArrayList<Object> jsonArray, Map<String, Object> flatMap, String keyBase) {
+
+            if(jsonArray == null) {
+                return;
+            }
+
+            for (int i = 0; i < jsonArray.size(); i++) {
+                if (jsonArray.get(i) instanceof LinkedHashMap) {
+                    new ResponseParserObject((LinkedHashMap<String, Object>) jsonArray.get(i),
+                            flatMap,
+                            String.format("%s[%d].", keyBase, i));
+                } else if (jsonArray.get(i) instanceof ArrayList) {
+                    new ResponseParserArray((ArrayList<Object>) jsonArray.get(i),
+                            flatMap,
+                            String.format("%s[%d]", keyBase, i));
+                } else {
+                    flatMap.put(String.format("%s[%d]", keyBase, i), jsonArray.get(i));
+                }
+            }
+        }
+    }
+
+}
--- src/main/java/eu/coinform/gateway/config/RuleEngineConfig.java
+++ src/main/java/eu/coinform/gateway/config/RuleEngineConfig.java
@@ -1,11 +1,14 @@
 package eu.coinform.gateway.config;
 
+import eu.coinform.gateway.cache.ModuleResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import rule.engine.RuleEngine;
 import rule.engine.RuleEngineFactory;
+
+import java.util.Map;
 
 @Configuration
 @Slf4j
