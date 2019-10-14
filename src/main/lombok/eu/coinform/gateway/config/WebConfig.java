64b83be75b7e829f065ce2e72689e2105425bcbc
daffyDukk <daniel.p.forsberg@gmail.com>
2019-10-14 15:43:04 +0200

    added CORS for chrome://*

--- src/main/lombok/eu/coinform/gateway/config/WebConfig.java
+++ src/main/lombok/eu/coinform/gateway/config/WebConfig.java
@@ -2,7 +2,6 @@ package eu.coinform.gateway.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -13,6 +12,12 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/twitter/**")
                 .allowedOrigins("https://twitter.com");
         registry.addMapping("/response/**")
+                .allowedOrigins("https://twitter.com");
+        // added below as in Slack. dont know if correct though. Have added a second *
+        // but not pushed to docker yet
+        registry.addMapping("chrome://**")
+                .allowedOrigins("https://twitter,com");
+        registry.addMapping("chrome-extension://**")
                 .allowedOrigins("https://twitter.com");
     }
 }
