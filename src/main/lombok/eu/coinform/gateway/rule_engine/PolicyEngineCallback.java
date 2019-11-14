d250d14e0a05c85ef52fee4c4acb5aa7b833b1bd
Andreas <andreas.berg@dsv.su.se>
2019-11-14 09:34:39 +0100

    averageCredibility method

--- src/main/lombok/eu/coinform/gateway/rule_engine/PolicyEngineCallback.java
+++ src/main/lombok/eu/coinform/gateway/rule_engine/PolicyEngineCallback.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 import model.Credibility;
 import rule.engine.Callback;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -19,5 +20,15 @@ public class PolicyEngineCallback implements Callback {
     public PolicyEngineCallback() {
         moduleCredibility = new HashMap<>();
         this.finalCredibility = Credibility.not_verifiable_post;
+    }
+
+    public Credibility averageCredibility(Collection<Credibility> credibilityLabels) {
+        int cred_sum = 0;
+        int cred_count = 0;
+        for (Credibility cred : credibilityLabels) {
+            cred_sum += cred.ordinal();
+            cred_count++;
+        }
+        return Credibility.values()[cred_sum/cred_count];
     }
 }
