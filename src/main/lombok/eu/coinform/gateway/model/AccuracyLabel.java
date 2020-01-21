a7003c36baef9622034b323350930d010f02856a
Andreas <andreas.j.berg@gmail.com>
2020-01-21 17:28:25 +0100

    json formatting fix

--- src/main/lombok/eu/coinform/gateway/model/AccuracyLabel.java
+++ src/main/lombok/eu/coinform/gateway/model/AccuracyLabel.java
@@ -3,17 +3,10 @@ package eu.coinform.gateway.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public enum AccuracyLabel {
-    @JsonProperty("accurate")
     accurate,
-    @JsonProperty("accurate with considerations")
     accurate_with_considerations,
-    @JsonProperty("unsubstantiated")
     unsubstantiated,
-    @JsonProperty("inaccurate with considerations")
     inaccurate_with_considerations,
-    @JsonProperty("inaccurate")
     inaccurate,
-    @JsonProperty("not verifiable")
     not_verifiable;
-
 }
